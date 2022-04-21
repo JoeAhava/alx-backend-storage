1c59964f326199ef04761bfe3f0f8b78a9e28c0b
Yohannes Terfa <lightshadow011@gmail.com>
2022-04-21 14:42:13 +0300

    create users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,5 @@
-/**
- create users table if not exists
- */
+-- create users table if not exists
+-- users
 CREATE TABLE IF NOT EXISTS users(
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
